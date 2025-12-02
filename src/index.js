3fe9e6ad4c5df2ca8b96396d8dd33063e4791baf
Thiện Nhân <nhanntn2203@gmail.com>
2025-12-02 15:16:39 +0700

    chatbot api key grok free by 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import homepage from "../index.html";
+
 export default {
   async fetch(request, env, ctx) {
-    return new Response('Hello World!');
+    return new Response(homepage, {
+      headers: {
+        "Content-Type": "text/html;charset=UTF-8",
+      },
+    });
   },
 };
